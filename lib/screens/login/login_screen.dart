5465faace7167dcbfacdb54a2ed641d9d3089457
yasikhafi <yasikhafi@student.telkomuniversity.ac.id>
2025-01-12 11:04:10 +0700

    Remove unused imports and comments from login screen

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,6 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-
-import 'package:demo/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:demo/screens/core/constant/colors.dart';
 import 'package:demo/screens/core/constant/regex.dart';
